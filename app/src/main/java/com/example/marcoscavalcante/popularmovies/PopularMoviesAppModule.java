59471e2cce4f9f2b0d826c408fbd54926ca9f680
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2018-03-19 12:46:50 +0000

    Add back button for MovieDetails screen; Add parallax image on MovieDetails toolbar; Replace Picasso for Glide; Change icon of sorting button

---
+++ app/src/main/java/com/example/marcoscavalcante/popularmovies/PopularMoviesAppModule.java
@@ -0,0 +1,12 @@
+package com.example.marcoscavalcante.popularmovies;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by marcoscavalcante on 18/03/2018.
+ */
+
+@GlideModule
+public class PopularMoviesAppModule extends AppGlideModule {
+}
